74d345747fbe6e8a8bd55719c5deab4e2aa2c70d
Florian Briou <fbriou@gmail.com>
2018-06-27 18:29:54 +0200

    init section

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import section from './section.vue'
+
+const name = "_wwbuilder_A";
+
+const addComponent = function () {
+    if (window.vm) {
+        window.vm.addComponent(name, section);
+
+        return true;
+    }
+    return false;
+}
+
+if (!addComponent()) {
+    const iniInterval = setInterval(function () {
+        if (addComponent()) {
+            clearInterval(iniInterval);
+        }
+    }, 10);
+}
---
+++ src/section.vue
@@ -0,0 +1,201 @@
+<template>
+	<div class="_wwbuilder_A  " ww-min-height="100">
+        <div class="background" ww-object="data.background" ww-category="background">
+        </div>
+
+        <div ww-vertical-align-table ww-min-height="100">
+            <div class="contents">
+                <div class="row-container container-fluid" v-for="row in data.rows">
+                    <div ww-columns="row"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+	name: "_wwbuilder_A",
+	props: {
+		section: Object
+	},
+	methods: {
+		addRow: function () {
+			if (!this.section.data.rows) {
+					this.section.data.rows = [];
+			}
+
+			const defaultRow = {
+            config: {
+                count: 3,
+                xs: null,
+                sm: [{
+                    offset: 0,
+                    width: 33.2,
+                    borders: [
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                    ]
+                }, {
+                    offset: 0,
+                    width: 33.2,
+                    borders: [
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                    ]
+                }, {
+                    offset: 0,
+                    width: 33.2,
+                    borders: [
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                        {
+                            width: 0,
+                            color: "#000000",
+                            style: "solid"
+                        },
+                    ]
+                }
+                ],
+                md: null,
+                lg: null
+            },
+            data: [
+                {
+                    background: null,
+                    wwObjects: []
+                },
+                {
+                    background: null,
+                    wwObjects: []
+                },
+                {
+                    background: null,
+                    wwObjects: []
+                }
+            ]
+        };
+
+			const newRow = JSON.parse(JSON.stringify(defaultRow));
+			newRow.uniqueId = wwUtils.getUniqueId();
+
+			this.section.data.rows.push(newRow);
+		},
+
+		removeRow: function (row) {
+			const index = this.section.data.rows.indexOf(row);
+			this.section.data.rows.splice(index, 1);
+		},
+	},
+	beforeDestroy: function () {
+		
+	},
+	created: function () {
+		
+	},
+	mounted: function () {
+
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+._wwbuilder_A {
+    position: relative;
+}
+
+._wwbuilder_A .background {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+}
+
+._wwbuilder_A .contents {
+    position: relative;
+    width: 100%;
+}
+
+._wwbuilder_A .top-padding-edit {
+    height: 50px;
+    width: 100%;
+}
+
+._wwbuilder_A .ww-add-block-container {
+    position: relative;
+    text-align: center;
+    padding:15px;
+}
+
+._wwbuilder_A .ww-add-block {
+    position: relative;
+    height:100px;
+    width: 100px;
+    display: inline-block;
+}
+
+._wwbuilder_A .row-container {
+    width: 100%;
+    position: relative;
+}
+
+._wwbuilder_A .row-container.editing {
+    padding-left:100px;
+}
+
+._wwbuilder_A .row-container > .edit-button-top-left {
+    top:2px;
+    left:2px;
+}
+</style>
