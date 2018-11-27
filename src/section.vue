7c388dbe1c8874f5f01885a3659c994c44db8f28
Kramerbaf <nds.kram@gmail.com>
2018-11-27 16:58:58 +0100

    update

--- src/section.vue
+++ src/section.vue
@@ -6,7 +6,7 @@
 
         <wwObject v-bind:ww-object="section.data.background" class="background" ww-category="background"></wwObject>
 
-        <wwLayoutColumn tag='div' ww-default="ww-row" :ww-list="section.data.rows" class="wwobjects-wrapper">
+        <wwLayoutColumn tag='div' ww-default="ww-row" :ww-list="section.data.rows" class="wwobjects-wrapper" @ww-add="add($event)" @ww-remove="remove($event)">
             <wwObject v-for="row in section.data.rows" :key="row.uniqueId" v-bind:ww-object="row" ww-row-default-height="100"></wwObject>
         </wwLayoutColumn>
     </div>
@@ -34,11 +34,45 @@ export default {
         }
     },
     methods: {
+        initData() {
+            //Init objects
+            let needUpdate = false;
+            if (!this.section.data.background) {
+                this.section.data.background = wwLib.wwObject.getDefault({ type: 'ww-color', data: { color: 'white' } });
+                needUpdate = true;
+            }
+            if (_.isEmpty(this.section.data.rows)) {
+                this.section.data.rows = [];
+                needUpdate = true;
+            }
+            if (needUpdate) {
+                this.sectionCtrl.update(this.section);
+            }
+        },
+        add(options) {
+            if (_.isEmpty(this.section.data.rows)) {
+                this.section.data.rows = [];
+            }
+
+            this.section.data.rows.splice(options.index, 0, options.wwObject);
+
+            this.sectionCtrl.update(this.section);
+        },
+        remove(options) {
+            if (_.isEmpty(this.section.data.rows)) {
+                this.section.data.rows = [];
+            }
+
+            this.section.data.rows.splice(options.index, 1);
+
+            this.sectionCtrl.update(this.section);
+        }
     },
     beforeDestroy: function () {
 
     },
     created: function () {
+        this.initData();
     },
     mounted: function () {
     }
