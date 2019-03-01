cdfb7f1857b747c49d847f27062a4ce5164a0eee
Kramerbaf <nds.kram@gmail.com>
2019-03-01 14:33:21 +0100

    changed langs

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -3,50 +3,50 @@ module.exports = [
         name: 'landingjll',
         category: 'Landing',
         storeName: {
-            en_GB: "Landing JLL"
+            en: "Landing JLL"
         },
         storeDesc: {
-            en_GB: "Landing for JLL"
+            en: "Landing for JLL"
         }
     },
     {
         name: 'objectifsjll',
         category: 'Features',
         storeName: {
-            en_GB: "Objectifs JLL"
+            en: "Objectifs JLL"
         },
         storeDesc: {
-            en_GB: "Landing for JLL"
+            en: "Landing for JLL"
         }
     },
     {
         name: 'propositions',
         category: 'Features',
         storeName: {
-            en_GB: "Propositions"
+            en: "Propositions"
         },
         storeDesc: {
-            en_GB: "Landing for JLL"
+            en: "Landing for JLL"
         }
     },
     {
         name: 'footer',
         category: 'Footer',
         storeName: {
-            en_GB: "Footer"
+            en: "Footer"
         },
         storeDesc: {
-            en_GB: "Landing for JLL"
+            en: "Landing for JLL"
         }
     },
     {
         name: 'tooljll',
         category: 'Features',
         storeName: {
-            en_GB: "JLL Tools"
+            en: "JLL Tools"
         },
         storeDesc: {
-            en_GB: "JLL Tools"
+            en: "JLL Tools"
         }
     }
 ]
