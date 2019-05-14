23778f4307603e0cf091acb7150b7e790e8b05ce
chihabeeddine <chihabeeddine.daoud@epitech.eu>
2019-05-14 18:21:09 +0200

    new templates

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -1,52 +1,132 @@
 module.exports = [
     {
+        name: 'cards',
+        category: 'Layout',
+        storeName: {
+            en: 'Section with Cards'
+        },
+        storeDesc: {
+            en: 'Section with Cards and description'
+        }
+    },
+    {
+        name: 'grid',
+        category: 'Mosaic',
+        storeName: {
+            en: 'Section with images'
+        },
+        storeDesc: {
+            en: 'Section with images'
+        }
+    },
+    {
         name: 'landing',
         category: 'Landing',
         storeName: {
-            en: "Landing"
+            en: 'Landing'
         },
         storeDesc: {
-            en: "section with logo and description"
+            en: 'section with logo and description'
         }
     },
     {
         name: 'landing2',
         category: 'Landing',
         storeName: {
-            en: "Simple landing"
+            en: 'Simple landing'
         },
         storeDesc: {
-            en: "Simple section to start with columns"
+            en: 'Simple section to start with columns'
+        }
+    },
+    {
+        name: 'landing3',
+        category: 'Landing',
+        storeName: {
+            en: 'landing with a parallax'
+        },
+        storeDesc: {
+            en: 'Landing page with a parallax'
         }
     },
     {
         name: 'map1',
         category: 'Content',
         storeName: {
-            en: "Simple map section"
+            en: 'Simple map section'
         },
         storeDesc: {
-            en: "Display a map with markers"
+            en: 'Display a map with markers'
+        }
+    },
+    {
+        name: 'parallaxLeft',
+        category: 'Parallax',
+        storeName: {
+            en: 'Section with a parallax'
+        },
+        storeDesc: {
+            en: 'Section with a parallax and text on the left'
+        }
+    },
+    {
+        name: 'parallaxRight',
+        category: 'Parallax',
+        storeName: {
+            en: 'Section with a parallax'
+        },
+        storeDesc: {
+            en: 'Section with a parallax and text on the right'
+        }
+    },
+    {
+        name: 'register',
+        category: 'Form',
+        storeName: {
+            en: 'Section with a register form'
+        },
+        storeDesc: {
+            en: 'Section with a register form'
+        }
+    },
+    {
+        name: 'sliderLeft',
+        category: 'Layout',
+        storeName: {
+            en: 'Slider on the left part'
+        },
+        storeDesc: {
+            en: 'Slider on the left part with info on the right'
+        }
+    },
+    {
+        name: 'sliderRight',
+        category: 'Layout',
+        storeName: {
+            en: 'Slider on the right part'
+        },
+        storeDesc: {
+            en: 'Slider on the right part with info on the left'
         }
     },
     {
         name: 'story',
         category: 'Features',
         storeName: {
-            en: "Story"
+            en: 'Story'
         },
         storeDesc: {
-            en: "section with image and texts to pass a message"
+            en: 'section with image and texts to pass a message'
         }
     },
     {
         name: 'footer',
         category: 'Footer',
         storeName: {
-            en: "Footer"
+            en: 'Footer'
         },
         storeDesc: {
-            en: "to end the page"
+            en: 'to end the page'
         }
     }
 ]
